1b6be098e0ce8f7b7db9e3815182430d2c38d631
Grave <pmpiga1@gmail.com>
2023-01-05 06:46:41 -0500

    new file:   installer/install.sh renamed:    installer/main.iss -> installer/installer.iss

---
+++ installer/install.sh
@@ -0,0 +1,23 @@
+novadir="/opt/Nova/"
+input=""
+ 
+read -p "What directory should Nova install to? (defaults to $novadir) " input
+
+if [ -n "$input" ] # if input is not empty
+then
+  novadir=$input
+fi
+
+echo "Installing to $novadir"
+
+mkdir -p $novadir
+wget -q -O "$novadir/nova" https://github.com/neroist/Nova/releases/download/1.6.0/nova
+
+echo "Nova has been installed.\n"
+
+echo "Please add Nova to your PATH. You can do so by adding this line at the end of your .bashrc or .profile file"
+echo "      export PATH="$novadir:\$PATH""
+
+echo "\nOr if you're using fish shell do:"
+echo "      set -U fish_user_paths $novadir \$fish_user_paths"
+echo "in a terminal"
