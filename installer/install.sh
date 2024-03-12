7b0c5d2fa556b842ad6cf9b0e5ef35b9832a0b16
neroist <pmpiga1@gmail.com>
2024-03-12 14:51:39 -0400

    improve linux installer

--- installer/install.sh
+++ installer/install.sh
@@ -1,23 +1,35 @@
-novadir="/opt/nova/"
+#!/bin/bash
+
+# set -e
+
+# check if sudo
+if [ "$(id -u)" -ne 0 ]; then
+  printf "Please run as root/sudo!\n"
+  exit 1
+fi
+
+# vars
+novaver="1.7.0"
+novadir="/opt/nova"
 input=""
- 
+
 read -p "What directory should Nova install to? (defaults to $novadir) " input
 
-if [ -n "$input" ] # if input is not empty
-then
+if [ -n "$input" ]; then # if input is not empty...
   novadir=$input
 fi
 
-echo "Installing to $novadir"
+printf "Installing to $novadir/\n\n"
 
 mkdir -p $novadir
-wget -q -O "$novadir/nova" https://github.com/neroist/nova/releases/download/1.7.0/nova
 
-echo "Nova has been installed.\n"
+wget -q -O "$novadir/nova" "https://github.com/neroist/nova/releases/download/$novaver/nova"
 
-echo "Please add Nova to your PATH. You can do so by adding this line at the end of your .bashrc or .profile file"
-echo "      export PATH="$novadir:\$PATH""
+printf "Nova has been installed.\n\n"
+printf "Please add Nova to your PATH. You can do so by adding this line at the end of your ~/.bashrc or ~/.profile file:\n"
+printf "      export PATH=\"$novadir:\$PATH\"\n"
+printf "Or if you're using fish shell do:\n"
+printf "      set -U fish_user_paths $novadir \$fish_user_paths\n"
+printf "in a terminal.\n"
 
-echo "\nOr if you're using fish shell do:"
-echo "      set -U fish_user_paths $novadir \$fish_user_paths"
-echo "in a terminal"
+chmod +x "$novadir/nova"
