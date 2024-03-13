d4d2e0293b26ff4b1d11463f2c01c2f47463beb4
neroist <pmpiga1@gmail.com>
2024-03-13 16:32:52 -0400

    improve install script (now sets file perms)

--- installer/install.sh
+++ installer/install.sh
@@ -22,8 +22,20 @@ fi
 printf "Installing to $novadir/\n\n"
 
 mkdir -p $novadir
-
 wget -q -O "$novadir/nova" "https://github.com/neroist/nova/releases/download/$novaver/nova"
+
+# make symlink to nova in secure path
+ln -s /usr/local/nova/nova /usr/local/bin/nova
+
+# create files needed by nova and apply permissions
+touch "$novadir/.KEY"
+touch "$novadir/.DEVICES"
+
+chmod u+wr,o+wr "$novadir/.DEVICES"
+chmod u+wr,o+wr "$novadir/.KEY"
+
+# apply perms to executable too
+chmod u+rx,o+rx "$novadir/nova"
 
 printf "Nova has been installed.\n\n"
 printf "Please add Nova to your PATH. You can do so by adding this line at the end of your ~/.bashrc or ~/.profile file:\n"
@@ -31,5 +43,3 @@ printf "      export PATH=\"$novadir:\$PATH\"\n"
 printf "Or if you're using fish shell do:\n"
 printf "      set -U fish_user_paths $novadir \$fish_user_paths\n"
 printf "in a terminal.\n"
-
-chmod +x "$novadir/nova"
