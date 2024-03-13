6ca3ec44220d50adbde6bee580c6565962c83948
neroist <pmpiga1@gmail.com>
2024-03-13 11:19:49 -0400

    indent doc comment and remove useless brackets

--- src/commands/devices.nim
+++ src/commands/devices.nim
@@ -15,12 +15,12 @@ proc devices* =
   for dev, i in devices.getElems():
     let 
       cmds = collect(for i in i["supportCmds"]: i.getStr())
-      ## seq of all supported commands of the device
+        ## seq of all supported commands of the device
 
     echo "\e[1m", "DEVICE ", $dev, ansiResetCode
     echo "  Mac Address: ", i["device"].getStr()
     echo "  Model: ", i["model"].getStr()
     echo "  Device Name: ", i["deviceName"].getStr()
-    echo "  Controllable: ", capitalizeAscii($(i["controllable"].getBool()))
-    echo "  Retrievable: ", capitalizeAscii($(i["retrievable"].getBool()))
+    echo "  Controllable: ", capitalizeAscii($i["controllable"].getBool())
+    echo "  Retrievable: ", capitalizeAscii($i["retrievable"].getBool())
     echo "  Supported Commands: ", cmds.join(", "), "\n"
